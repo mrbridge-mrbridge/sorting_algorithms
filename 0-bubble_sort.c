3c621f4295ba3fb45510e3dd1b1932046361a162
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-10-25 10:38:50 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 /**
  * bubble_sort - sorts in buble sort algorithm
---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
