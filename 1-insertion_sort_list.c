0243ad565a9935956841957d89d3d1aa0c767acb
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-10-26 04:20:17 -0700

    update

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a list using the insertion
+ * sort algorithm
+ * @list: pointer to pointer of the list to be sorted
+ *
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	int i, j;
+
+}
