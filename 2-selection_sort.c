5f5bfeacfcf0ee1a7b284d73eab5983021baf7bf
root <nanamcroj@gmail.com>
2021-10-26 17:21:46 -0700

    select sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array using the selection 
+ * sort algorithm
+ * @array: array to be sorted
+ * @size: array size
+ *
+ * Return: Nothing
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp;
+
+	if (size < 2 || array == NULL)
+		return;
+
+
+	for (i = 0; i < size - 1; i++)
+	{
+		k = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[k])
+				k = j;
+		}
+		if (i != k)
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
+	}
+}
